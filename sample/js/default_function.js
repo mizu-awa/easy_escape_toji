78ff39dcf7d860102e42863a49b9af2c5074b6b4
mizu-awa <mizuawakg@gmail.com>
2020-07-19 15:00:53 +0900

    新規作成

---
+++ sample/js/default_function.js
@@ -0,0 +1,131 @@
+﻿/* デフォルト関数（原則、このファイルは変更しない） */
+/* ユーザーが使用できる関数と、使用できない関数に分ける？ */
+
+/* シーン遷移(シーン名) */
+function moveScene(scene){
+
+	//document.getElementById("")
+
+}
+
+/* 重なり判定(オブジェクト1,オブジェクト2) */
+function checkOverlap(obj1,obj2){
+
+}
+
+/* アイテムを取得(アイテム名) */
+function getItem(item){
+}
+
+/* アイテムを破棄(アイテム名) */
+function discardItem(item){
+}
+
+/* アイテムを選択(アイテム名) */
+function selectItem(item){
+}
+
+/* アイテムウィンドウを開く() */
+function openItemWindow(){
+}
+
+/* アイテムウィンドウを閉じる() */
+function closeItemWindow(){
+}
+
+
+/* BGM開始(ファイル名,ループ有無) */
+function 	playBGM(fileName,roop){
+}
+
+/* BGM停止() */
+function stopBGM(){
+}
+
+/* SE再生(ファイル名) */
+function playSE(fileName){
+}
+
+/* 音量を上げる() */
+function upVolume(){
+}
+
+/* 音量を下げる() */
+function downVolume(){
+}
+
+/* 音量を変える(数値) */
+function changeVariable(variable,value){
+}
+
+/* 変数に値をセット(変数名,値) */
+function setValue(variable,value){
+}
+
+/* 変数の値を参照(変数名) */
+function getValue(variable){
+}
+
+/* タイマ開始/再開(タイマ名) */
+function startTimer(timer){
+}
+
+/* タイマを一時停止(タイマ名) */
+function stopTimer(timer){
+}
+
+/* タイマの現在カウント(タイマ名) */
+function getCountTimer(timer){
+}
+
+/* タイマの残り時間(タイマ名) */
+function getRemainingCountTimer(timer){
+}
+
+/* 現在時刻を表示() */
+function getNow(){
+}
+
+/* アニメーション */
+
+
+/* セーブ(スロット番号) */
+function save(num){
+}
+
+/* ロード(スロット番号) */
+function load(){
+}
+
+
+/* テキストを再生(テキスト) */
+function textBox(text){
+}
+
+/* テキストを再生(名前,テキスト) */
+function textBox(name,text){
+	textBox(text);
+}
+
+/* テキストを再生(名前,人物画像,テキスト) */
+function textBox(name,img,text){
+	textBox(name,text);
+}
+
+/* 立ち絵を表示(立ち絵位置,画像ファイル) */
+function showCharacterImage(crl,img){
+	
+}
+
+/* 立ち絵を消去(立ち絵位置) */
+function deleteCharacterImage(crl){
+}
+
+/* マウスの座標を取得 */
+function getMousePointX(){
+}
+function getMousePointY(){
+}
+
+/* マウスとオブジェクトの重なり判定 */
+
